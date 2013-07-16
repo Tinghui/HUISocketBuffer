20c7dcd1dc682db4fa9402b4eba0d5b3b77dcd62
Tinghui <tinghui.zhang3@gmail.com>
2013-07-16 22:30:37 +0800

    Initial Commit

---
+++ HUISocketBuffer/HUISocketBuffer.h
@@ -0,0 +1,13 @@
+//
+//  HUISocketBuffer.h
+//  HUISocketBuffer
+//
+//  Created by ZhangTinghui on 13-7-16.
+//  Copyright (c) 2013年 ZhangTinghui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HUISocketBuffer : NSObject
+
+@end
---
+++ HUISocketBuffer/HUISocketBuffer.m
@@ -0,0 +1,13 @@
+//
+//  HUISocketBuffer.m
+//  HUISocketBuffer
+//
+//  Created by ZhangTinghui on 13-7-16.
+//  Copyright (c) 2013年 ZhangTinghui. All rights reserved.
+//
+
+#import "HUISocketBuffer.h"
+
+@implementation HUISocketBuffer
+
+@end
